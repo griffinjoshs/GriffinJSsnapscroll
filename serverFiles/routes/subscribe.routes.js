1510d89b4abfc8a25e870ec4a07b8a7db2606009
griffinjoshs <leveloutusa@gmail.com>
2022-04-08 17:59:40 -0400

    i am removed

--- serverFiles/routes/subscribe.routes.js
+++ serverFiles/routes/subscribe.routes.js
@@ -2,6 +2,5 @@ const subscribeController = require("../controllers/subscribe.controller");
 
 module.exports = function (app) {
     app.get('/api', subscribeController.index)
-    console.log('routeFile')
     app.post('/api/subscribe', subscribeController.newSubscriber)
 }
